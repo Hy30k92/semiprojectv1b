bff0085bad6e1f437b9ad29bb7fef62f326f7bfa
Hy30k92 <nakmh92@gmail.com>
2024-08-27 11:59:34 +0900

    gallery list 관련 코드 추가 gallery view 관련 코드v1 추가

--- app/service/gallery.py
+++ app/service/gallery.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 from fastapi import Form
 from sqlalchemy import insert, select
+from sqlalchemy.orm import aliased
 
 from app.model.gallery import GalAttach, Gallery
 from app.schema.gallery import NewGallery
@@ -69,4 +70,21 @@ class GalleryService:
 
         except Exception as ex:
             print(f' ▶▶▶ select_gallery 에서 오류 발생: {str(ex)} ')
+            db.rollback()
+
+    @staticmethod
+    def selectone_gallery(gno, db):
+        try:
+           stmt = select(Gallery).where(Gallery.gno == gno)
+           result1 = db.execute(stmt).first()
+
+           ga = aliased(GalAttach)
+           stmt = select(ga.fname, ga.fsize).where(ga.gno == gno)
+           result2 = db.execute(stmt).fetchall()
+
+           return result1, result2
+
+
+        except Exception as ex:
+            print(f' ▶▶▶ selectone_gallery 에서 오류 발생: {str(ex)} ')
             db.rollback()
