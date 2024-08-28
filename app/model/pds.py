8d268e695cd4325998baa526ac9350758f28f0c8
Hy30k92 <nakmh92@gmail.com>
2024-08-28 10:48:40 +0900

    pds 모델 관련 파일 추가

---
+++ app/model/pds.py
@@ -0,0 +1,33 @@
+from sqlalchemy import ForeignKey
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.model.base import Base
+
+# 양방향 관계설정
+class Gallery(Base):
+    __tablename__ = 'pds'
+
+    pno: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
+    title: Mapped[str] = mapped_column(index=True)
+    userid: Mapped[str] = mapped_column(ForeignKey('member.userid'), index=True) # 비식별관계 -> 식별관계
+    regdate: Mapped[datetime] = mapped_column(default=datetime.now)
+    views: Mapped[int] = mapped_column(default=0)
+    contents: Mapped[str]
+    attachs = relationship('PdsAttach', back_populates='pds') # 하나의 gallery 는 하나 이상의 attach 가 존재 ( 1:n )
+
+class PdsAttach(Base):
+    __tablename__ = 'pdsattach'
+
+    pano: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
+    pno: Mapped[int] = mapped_column(ForeignKey('pds.pno'), index=True)
+    fname: Mapped[str] = mapped_column(nullable=False)
+    fsize: Mapped[int] = mapped_column(default=0)
+    regdate: Mapped[datetime] = mapped_column(default=datetime.now)
+    gallery = relationship('Pds', back_populates='attachs') # 하나의 attach 는 하나의 gallery 에 속함 ( 1:1 )
+
+
+
+
+
