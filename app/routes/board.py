531da8739a0e8dc25effff43ace1f8df2bea80a6
Hy30k92 <nakmh92@gmail.com>
2024-08-28 14:15:00 +0900

    검색 관련 기능구현

--- app/routes/board.py
+++ app/routes/board.py
@@ -52,5 +52,18 @@ async def write(req: Request):
 
 
 @board_router.get('/view/{bno}', response_class=HTMLResponse)
-async def view(req: Request ):
+async def view(req: Request):
     return templates.TemplateResponse('board/view.html', {'request': req})
+
+
+@board_router.get('/list/{ftype}/{fkey}/{cpg}', response_class=HTMLResponse)
+async def find(req: Request, ftype: str, fkey: str, cpg: int, db: Session = Depends(get_db)):
+    try:
+        stpgb = int((cpg - 1) / 10) * 10 + 1
+        bdlist = BoardService.find_select_board(db, ftype, '%'+fkey+'%', cpg)
+        return templates.TemplateResponse('board/list.html',
+                                          {'request': req, 'bdlist': bdlist, 'cpg': cpg, 'stpgb': stpgb})
+
+    except Exception as ex:
+        print(f' ▷▷▷ find 오류발생 : {str(ex)}')
+        return RedirectResponse(url='/member/error', status_code=303)
