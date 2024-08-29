1b63e16c0a96f216368ddfffba56101fa3b27250
Hy30k92 <nakmh92@gmail.com>
2024-08-29 10:17:46 +0900

    댓글 ㄹ모델 추가 및 1:N 관계 설정

--- app/model/board.py
+++ app/model/board.py
@@ -1,7 +1,7 @@
 from sqlalchemy import ForeignKey
 from datetime import datetime
 
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.model.base import Base
 
@@ -15,9 +15,18 @@ class Board(Base):
     regdate: Mapped[datetime] = mapped_column(default=datetime.now)
     views: Mapped[int] = mapped_column(default=0)
     contents: Mapped[str]
+    replys = relationship('Reply', back_populates='board')
 
+class reply(Base):
+    __tablename__ = 'board'
 
-
+    rno: [int] = mapped_column(primary_key=True, autoincrement=True, index=True)
+    rerply: Mapped[str] = mapped_column(index=True)
+    userid: Mapped[str] = mapped_column(ForeignKey('member.userid'), index=True) # 비식별관계 -> 식별관계
+    regdate: Mapped[datetime] = mapped_column(default=datetime.now)
+    bno: Mapped[int] = mapped_column(ForeignKey('board.bno')),
+    rpno: Mapped[int] = mapped_column(ForeignKey('reply.bno')),
+    board = replys = relationship('Board', back_populates='replys')
 
 
 
