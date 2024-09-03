d55ad35bc30e4d1b830782b76b868594d5380800
Hy30k92 <nakmh92@gmail.com>
2024-09-03 10:44:45 +0900

    sqlite모델을 mariadb로 마이그레이션

--- app/model/gallery.py
+++ app/model/gallery.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, String, Text
 from datetime import datetime
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
@@ -10,19 +10,19 @@ class Gallery(Base):
     __tablename__ = 'gallery'
 
     gno: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
-    title: Mapped[str] = mapped_column(index=True)
-    userid: Mapped[str] = mapped_column(ForeignKey('member.userid'), index=True) # 비식별관계 -> 식별관계
+    title: Mapped[str] = mapped_column(String(100),index=True)
+    userid: Mapped[str] = mapped_column(String(18),ForeignKey('member.userid'), index=True) # 비식별관계 -> 식별관계
     regdate: Mapped[datetime] = mapped_column(default=datetime.now)
     views: Mapped[int] = mapped_column(default=0)
-    contents: Mapped[str]
-    attachs = relationship('GalAttach', back_populates='gallery') # 하나의 gallery 는 하나 이상의 attach 가 존재 ( 1:n )
+    contents: Mapped[str] = mapped_column(Text)
+    attachs = relationship('GalAttach', back_populates='gallery') #하나의 gallery 는 하나 이상의 attach 가 존재 ( 1:n )
 
 class GalAttach(Base):
     __tablename__ = 'galattach'
 
     gano: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
     gno: Mapped[int] = mapped_column(ForeignKey('gallery.gno'), index=True)
-    fname: Mapped[str] = mapped_column(nullable=False)
+    fname: Mapped[str] = mapped_column(String(35),nullable=False)
     fsize: Mapped[int] = mapped_column(default=0)
     regdate: Mapped[datetime] = mapped_column(default=datetime.now)
     gallery = relationship('Gallery', back_populates='attachs') # 하나의 attach 는 하나의 gallery 에 속함 ( 1:1 )
